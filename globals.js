30ed76855b82acc9bc6497101ab29d1e40d4a630
Ultra Loli 64 <psostorm@gmail.com>
2020-04-24 16:23:58 +0200

    I need a small watermeloan

--- globals.js
+++ globals.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet, Dimensions, PermissionsAndroid } from 'react-native';
 
 export var deviceHeight = Dimensions.get('window').height;
 export var deviceWidth = Dimensions.get('window').width;
@@ -15,6 +15,28 @@ export const serverUrl = `192.168.1.15:5000`
 export const apiUrl = `http://${serverUrl}/api/v1`;
 
 export const MapsApiRootUrl = 'https://maps.googleapis.com/maps/api/staticmap?';
+
+export const getDistBetweenCoords = (lat1, lon1, lat2, lon2, unit) => {
+    if ((lat1 == lat2) && (lon1 == lon2)) {
+        return 0;
+    }
+    else {
+        var radlat1 = Math.PI * lat1 / 180;
+        var radlat2 = Math.PI * lat2 / 180;
+        var theta = lon1 - lon2;
+        var radtheta = Math.PI * theta / 180;
+        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        if (dist > 1) {
+            dist = 1;
+        }
+        dist = Math.acos(dist);
+        dist = dist * 180 / Math.PI;
+        dist = dist * 60 * 1.1515;
+        if (unit == "K") { dist = dist * 1.609344 }
+        if (unit == "N") { dist = dist * 0.8684 }
+        return dist;
+    }
+}
 
 export const gs = StyleSheet.create({
 
