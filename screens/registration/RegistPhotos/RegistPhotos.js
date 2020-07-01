c39e115528e6f16d06314526bb327304da35b821
Rosy <rosybel666diaz@gmail.com>
2020-07-01 15:45:04 +0200

    lep

--- screens/registration/RegistPhotos/RegistPhotos.js
+++ screens/registration/RegistPhotos/RegistPhotos.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 import { RegistStyles } from '../../../styles/RegistStyles';
 
 import Body, { FlexSection } from '../../../components/Body';
-import WaitIndicator from '../../../components/waitIndicator';
+// import WaitIndicator from '../../../components/waitIndicator';
 import UpForMeButton from '../../../components/UpForMeButton';
 import RegistUp4MeLogo from '../../../components/LoginAndRegistration/RegistUp4MeLogo';
 import RegistHeader from '../../../components/LoginAndRegistration/RegistHeader';
+import TextQuicksand from '../../../components/TextQuicksand';
+import NetworkFeedBackIndicator from '../../../components/waitIndicator';
 
 const RegistPhotos = () => {
 
-    const [busy, setBusy] = useState(false);
+    const [netFeedback, setNetFeedback] = useState({
+        busy: false,
+        message: '',
+    });
 
     return (
         <Body>
@@ -19,18 +24,35 @@ const RegistPhotos = () => {
                 <RegistUp4MeLogo />
                 <RegistHeader>Foto's toevoegen</RegistHeader>
 
-                <View style={RegistStyles.container}>
+                <View style={[RegistStyles.container,]}>
 
+                    <TextQuicksand style={styles.underline} onPress={() => { openBrowser('https://www.uptodates.nl/richtlijnen-veiligheid') }}>Lees de richtlijnen</TextQuicksand>
                 </View>
             </FlexSection>
 
             <View style={RegistStyles.bottom}>
-                <WaitIndicator style={RegistStyles.waitIndicator} visible={busy} />
-                <UpForMeButton title={'doorgaan'} enabled={false} onPress={async () => { }} />
+                <NetworkFeedBackIndicator style={RegistStyles.waitIndicator} message={netFeedback.message} />
+                <UpForMeButton style={RegistStyles.botButton} title={'doorgaan'} enabled={false} onPress={async () => {
+                    setNetFeedback({
+                        busy: true,
+                        message: networkFeedbackMessages.wait,
+                    })
+
+                    if (devMode.network) {
+                        console.log('request:', getEndpoint(endpoints.something));
+                    }
+                }} />
             </View>
 
         </Body>
     );
 }
+const styles = StyleSheet.create({
+
+    underline: {
+        textDecorationLine: "underline",
+
+    },
+});
 
 export default RegistPhotos;
